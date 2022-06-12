e45c0bd031b6a48e8b0230efbbf251c91e70a676
Maur Zac <maur.mx@gmail.com>
2022-06-11 23:26:25 -0500

    Initial Commit

---
+++ SUCombineMVVM/SUCombineMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUCombineMVVMApp.swift
+//  SUCombineMVVM
+//
+//  Created by Mauricio Zarate on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SUCombineMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
